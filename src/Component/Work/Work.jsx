766f64455adca1ccb80651470facf206e20fe989
ahmed <ahmedjones123@gmail.com>
2023-02-08 20:27:18 -0600

    added new project

--- src/Component/Work/Work.jsx
+++ src/Component/Work/Work.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import './work.css'
 import grocery from './Pictures/grocery.png'
 import huluclone from './Pictures/huluclone.png'
+import kanbanTask from "./Pictures/Kanban Project.png"
 
 function Work() {
     return (
         <div className='work_container' id="portfolio">
             <h1>Projects</h1>
             <div className="project_wrapper"> 
+            <div className="projects">
+                <img src={kanbanTask} alt="project" className='project_pic'/>
+                <h2 className='project_title'> Task Managment </h2>
+                <p className='project_detail'>Kanban Task Managment </p>
+                <div className="visit">
+                <a href="https://kanban-project-task-managment.vercel.app/">Visit Page</a>
+                <a href="https://github.com/ahmed19567/Kanban-Project/tree/master/kanban-app">Code</a>
+                </div>
+            </div>
             <div className="projects">
                 <img src={grocery} alt="project" className='project_pic'/>
                 <h2 className='project_title'>Grocery Project</h2>
@@ -26,6 +36,7 @@ function Work() {
                 <a href="https://github.com/ahmed19567/HuluClone">Code</a>
                 </div>
             </div>
+          
             </div>
             
         </div>
